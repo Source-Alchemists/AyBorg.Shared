15ef92015bf453dff62f29e024b261bcdcebd227
Levent Tasdemir <light4code@gmail.com>
2024-10-06 15:41:49 +0200

    Added Authrorization package

---
+++ Authorization/src/Roles.cs
@@ -0,0 +1,9 @@
+namespace AyBorg.SDK.Authorization;
+
+public static class Roles
+{
+    public const string Administrator = "Administrator";
+    public const string Engineer = "Engineer";
+    public const string Auditor = "Auditor";
+    public const string Reviewer = "Reviewer";
+}
